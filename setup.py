d5389d767da6a1e2a33f35dc89dad8520334bba3
Chengxin Yang <52123132+Cheungsin@users.noreply.github.com>
2021-04-28 23:41:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="STA663-final-biclustering-cxgq", # Replace with your own username
+    name="SSVD", # Replace with your own username
     version="1.0",
     author="Chengxin Yang, Guanqi Zeng",
     author_email="chengxin.yang@duke.edu, guanqi.zeng@duke.edu",
     description='A python package of Biclustering for Sparse matrix',
     url="https://github.com/Cheungsin/STA663-final-biclustering",
-    py_modules = ['STA-663-final-biclustering-cxgq'],
+    py_modules = ['SSVD'],
     classifiers=[
         "Programming Language :: Python :: 3",
         'Programming Language :: Python :: 3.4',
@@ -23,7 +23,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"SSVD": "SSVD"},
-    packages=setuptools.find_packages(),
+    packages=['SSVD'],
     python_requires=">=3",
 )
